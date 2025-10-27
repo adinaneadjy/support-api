6cc9d053a3c016368cb4e0cfbafd0427989ed64c
Adinaneadjy <aadradjydjoubeiri@gmail.com>
2025-10-27 19:28:45 +0100

    Correction formatage Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
   env: {
     node: true,
-    es2021: true
+    es2021: true,
   },
   extends: ["eslint:recommended", "plugin:prettier/recommended"],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: "module"
+    sourceType: "module",
   },
   rules: {
-    // j'ajouter mes règles 
-  }
+    // j'ajouter mes règles
+  },
 };
--- src/models/RequestType.js
+++ src/models/RequestType.js
@@ -1,17 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const requestTypeSchema = new mongoose.Schema(
   {
     code: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     description: { type: String, required: true },
-    priority: { type: String, enum: ['low', 'medium', 'high', 'critical'], default: 'medium' },
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high", "critical"],
+      default: "medium",
+    },
     category: { type: String, required: true },
     estimatedResponseTime: { type: Number },
     isActive: { type: Boolean, default: true },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
 // **exporter le modèle**
-module.exports = mongoose.model('RequestType', requestTypeSchema);
+module.exports = mongoose.model("RequestType", requestTypeSchema);
